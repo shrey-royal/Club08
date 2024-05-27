16172474549dfabf16546813bdcfdee5f7f74310
Shrey K <srkkadia@gmail.com>
2024-05-27 18:29:21 +0530

    Replace Word Method added

--- File Handling/ReadWrite.cpp
+++ File Handling/ReadWrite.cpp
@@ -76,6 +76,44 @@ public:
             cout << "Error opening the file..." << endl;
         }
     }
+
+    void replaceWord() {
+        ifstream file(fileName + ".txt");
+        ofstream tempFile("temp.txt");
+        string oldWord, newWord, line;
+        bool replaced = false;
+
+        cout << "\nEnter the word to be replaced: ";
+        cin >> oldWord;
+        cout << "\nEnter the new word: ";
+        cin >> newWord;
+        cin.ignore();
+
+        if(file.is_open() && tempFile.is_open()) {
+            while(getline(file, line)) {
+                size_t pos;    //size_t => unsigned int
+                while((pos = line.find(oldWord)) != string::npos) {
+                    line.replace(pos, oldWord.length(), newWord);
+                    replaced = true;
+                }
+                tempFile << line << endl;
+            }
+
+            file.close();
+            tempFile.close();
+
+            if(replaced) {
+                remove((fileName + ".txt").c_str());
+                rename("temp.txt", (fileName + ".txt").c_str());
+                cout << "Word \"" << oldWord << "\" replaced with \"" << newWord << "\" successfully." << endl;
+            } else {
+                system("pause");
+                cout << "Word \"" << oldWord << "\" not found in the life" << endl;
+            }
+        } else {
+            cout << "Error opening the file..." << endl;
+        }
+    }
 };
 
 int main() {
@@ -83,7 +121,7 @@ int main() {
     FileHandler fh;
 
     do {
-        cout << "\n1. Write File\n2. Read File\n3. Update File\n4. Find Word\n0. Exit" << endl;
+        cout << "\n1. Write File\n2. Read File\n3. Update File\n4. Find Word\n5. Replace Word\n0. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
         cin.ignore();
@@ -111,6 +149,10 @@ int main() {
 
             case 4:
                 fh.findWord();
+                break;
+
+            case 5:
+                fh.replaceWord();
                 break;
             
             default:
