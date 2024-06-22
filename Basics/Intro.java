7c818feba134053a8848ca63ccf751dfdbfe8642
Shrey K <srkkadia@gmail.com>
2024-06-22 12:05:56 +0530

    Basics

---
+++ Basics/Intro.java
@@ -0,0 +1,17 @@
+public class Intro {
+	public static void main(String[] args) {
+		String s = "Basic";
+		int a = 456;
+		char c = 'a';
+		//System.out.print("Hello, World!\na = " + a + "\nc = " + c + "\nString = " + s);
+		//System.out.println("2nd line");
+		//System.out.println("3rd line");
+		
+		String name = "Shrey";
+		float marks = 34.445f;
+		//System.out.println("Hello, My name is " + name + ", and I'm a Student.");
+		//System.out.println("I got " + marks + " in last semester.");
+		//System.out.printf("I got %.1f in last semester.", marks);
+		
+	}
+}
