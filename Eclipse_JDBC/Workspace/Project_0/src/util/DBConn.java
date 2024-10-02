4c0988ba7632f67aa66236267458c48c0f735466
Shrey K <srkkadia@gmail.com>
2024-10-02 20:06:18 +0530

    DbConnection

---
+++ Eclipse_JDBC/Workspace/Project_0/src/util/DBConn.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConn {
+	
+	private static String URL = "jdbc:mysql://localhost:3306/club08";
+	private static String USERNAME = "root";
+	private static String PASSWORD = "root";
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			
+			if(conn != null) {
+				System.out.println("Database Conected!");
+			} else {
+				System.out.println("Failed to connect to club08.");
+			}
+		} catch (Exception e) {
+			System.out.println("Error Connecting to the database: ");
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+//	public static void main(String[] args) {
+//		System.out.println(getConnection());
+//	}
+}
